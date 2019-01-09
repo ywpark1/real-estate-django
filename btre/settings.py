690619f63ebef4ea40d585c555da42b7ea223e84
Yeonwoo Park <ywoo1234@gmail.com>
2019-01-09 17:10:40 -0500

    Add media url and path

--- btre/settings.py
+++ btre/settings.py
@@ -124,3 +124,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'btre/static')]
+
+# Media Folder Setting
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
